652b9094a56af11d8f7580bfbb1d069edbf254e9
AlexisPV <apinto@ing.ucsc.cl>
2021-12-15 18:19:35 -0300

    ingresa entrada- salida

---
+++ mantenedor/admin/funciones/ingreso.php
@@ -0,0 +1,55 @@
+<?php 
+
+require "../../conexion/auth.php";
+
+if(isset($_POST["text"])){
+
+
+    // recuperamos patente
+    $patente = $_POST["text"];
+    echo $patente;
+    $query = "SELECT * from vehiculo where Patente_vehiculo = $patente";
+    $consulta = mysqli_query($conexion,$query);
+
+    if (!isset($consulta)) {
+        die("Patente no registrada");
+        header('location: ../scanner.php');
+    
+        }
+
+    else{
+
+        $query = "SELECT COUNT(*)+3 as ingreso from registro;";
+        $consulta = mysqli_query($conexion,$query);
+        $ROW = mysqli_fetch_array($consulta);
+        $registro = $ROW['ingreso'];
+
+
+        // estacionamiento disponible
+        $query = "SELECT id_estacionamiento as id from estacionamiento where estado = 0
+        LIMIT 1";
+        $consulta = mysqli_query($conexion,$query);
+        $ROW = mysqli_fetch_array($consulta);
+        $estacionamiento = $ROW['id'];
+
+        // inserto registro primero
+        $sql = "INSERT INTO `registro`(`id_registro`, `hora_entrada`, `hora_salida`, `fk_id_estacionamiento`) VALUES ('$registro',NOW(),'','$estacionamiento')";
+        $res = mysqli_query($conexion, $sql);
+
+        if(!$res){
+            die("ingreso 1 error");
+        }
+        else{
+        
+            //ingresa 
+        $sql = "INSERT INTO `ingresa`(`fecha`, `fk_id_registro`, `fk_Patente_vehiculo`) VALUES (NOW(),'$registro','$patente')";
+        $resultado = mysqli_query($conexion, $sql);
+        }
+        
+        }
+} 
+
+
+
+
+?>
---
+++ mantenedor/admin/funciones/salir.php
@@ -0,0 +1,33 @@
+<?php 
+
+require "../../conexion/auth.php";
+
+if(isset($_POST["text"])){
+
+    $patente = $_POST["text"];  
+    // recuperamos ultimo registro de ingreso
+    $query = "SELECT MAX(fk_id_registro) as id from ingresa WHERE fk_Patente_vehiculo = '$patente'";
+    $consulta = mysqli_query($conexion,$query);
+    $ROW = mysqli_fetch_array($consulta);
+    $n_registro = $ROW['id'];
+
+    $sql = "UPDATE `registro` SET `hora_salida`= NOW() WHERE id_registro = $n_registro";
+    $res = mysqli_query($conexion, $sql);
+
+    if (!isset($consulta)) {
+        die("Patente no registrada");
+        header('location: ../scanner-inicio.php');
+    
+        }
+
+    else{
+
+    
+        
+    }
+} 
+
+
+
+
+?>
